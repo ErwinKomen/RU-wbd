348acfb0f12bb7d7a5f2492875faca8f82b66c5f
ErwinKomen <E.Komen@ru.nl>
2018-02-08 09:49:08 +0100

    Fixed wrong intro pdf

--- wbd/wbd/dictionary/views.py
+++ wbd/wbd/dictionary/views.py
@@ -1999,7 +1999,8 @@ class DeelListView(ListView):
         # Set the title of the application
         context['title'] = "e-WBD afleveringen"
 
-        context['intro_pdf'] = "wbd-1/2000_Brabantse Dialecten III Inleiding_Compleet.pdf"
+        context['intro_pdf'] = "wbd-1/1967_Brabantse Dialecten Voorlopige inleiding.pdf"
+        context['intro_op_drie_pdf'] = "wbd-3/2000_Brabantse Dialecten III Inleiding_Compleet.pdf"
 
         # Return the calculated context
         return context
